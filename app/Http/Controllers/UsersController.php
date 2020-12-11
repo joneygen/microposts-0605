9306b56e999a6cf54e812ea693f5b2d788522573
naoya.harata <joneygennumber@gmail.com>
2020-12-11 13:07:15 +0000

    timeline

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -83,5 +83,17 @@ class UsersController extends Controller
             'users' => $followers,
         ]);
     }
-}
+    
+    /**
+     * このユーザとフォロー中ユーザの投稿に絞り込む。
+     */
+    public function feed_microposts()
+    {
+        // このユーザがフォロー中のユーザのidを取得して配列にする
+        $userIds = $this->followings()->pluck('users.id')->toArray();
+        // このユーザのidもその配列に追加
+        $userIds[] = $this->id;
+        // それらのユーザが所有する投稿に絞り込む
+        return Micropost::whereIn('user_id', $userIds);
+    }
 }
